d7125eb11e238df10a66137c495a3b83f65775f5
Viola Resyli <violaresyli@gmail.com>
2025-05-17 10:05:41 +0200

    aes dhe rsa

--- src/crypto/AES.java
+++ src/crypto/AES.java
@@ -1,4 +1,35 @@
 package crypto;
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+
 public class AES {
+
+    private static final String AES_ALGO = "AES";
+    private static final String AES_CIPHER = "AES/CBC/PKCS5Padding";
+
+    public static SecretKey generateAESKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(AES_ALGO);
+        keyGen.init(256);
+        return keyGen.generateKey();
+    }
+
+    public static IvParameterSpec generateIV() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static byte[] encrypt(byte[] data, SecretKey key, IvParameterSpec iv) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES_CIPHER);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decrypt(byte[] encryptedData, SecretKey key, IvParameterSpec iv) throws Exception {
+        Cipher cipher = Cipher.getInstance(AES_CIPHER);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(encryptedData);
+    }
 }
--- src/crypto/RSA.java
+++ src/crypto/RSA.java
@@ -1,4 +1,38 @@
 package crypto;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.Cipher;
 
 public class RSA {
+
+    private static final String RSA_ALGO = "RSA";
+    private static final String CIPHER_TRANSFORMATION = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(RSA_ALGO);
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    public static byte[] encrypt(PublicKey publicKey, byte[] data) throws Exception {
+        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decrypt(PrivateKey privateKey, byte[] encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(encryptedData);
+    }
+
+    public static byte[] getPublicKeyBytes(PublicKey publicKey) {
+        return publicKey.getEncoded();
+    }
+
+    public static PublicKey getPublicKeyFromBytes(byte[] keyBytes) throws Exception {
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance(RSA_ALGO);
+        return kf.generatePublic(spec);
+    }
 }
